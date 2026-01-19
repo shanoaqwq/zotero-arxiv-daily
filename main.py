f950de606e550ebc94f946f59a50c441bc6f4b67
shanoaqwq <112261940+shanoaqwq@users.noreply.github.com>
2026-01-19 21:02:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -184,6 +184,7 @@ if __name__ == '__main__':
     else:
         logger.info("Reranking papers...")
         papers = rerank_paper(papers, corpus)
+        papers = [p for p in papers if p.score > 2]
         if args.max_paper_num != -1:
             papers = papers[:args.max_paper_num]
         if args.use_llm_api:
